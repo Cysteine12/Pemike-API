82362d0baa57d5573f9db4be13688ca8cb30c285
Cysteine12 <gbengapeace2001@gmail.com>
2025-05-04 20:55:50 +0100

    feat(admin/user): add user update and dashboard logic

---
+++ src/middlewares/validate.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express'
+import { ValidationError } from './errorHandler'
+import { Schema } from 'zod'
+
+const validate = (schema: Schema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body)
+      next()
+    } catch (err: any) {
+      throw new ValidationError(err.errors[0])
+    }
+  }
+}
+
+export { validate }
